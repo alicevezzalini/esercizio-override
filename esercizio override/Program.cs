6366ac8550cf7bb7b0c202f52d20b24e3ad3b74f
Utente <Utente@PC-DI-SIMO>
2024-01-24 20:00:40 +0100

    Aggiungere i file di progetto.

---
+++ esercizio override/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_override
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cerchio cerchio = new Cerchio();    
+            Rettangolo rettangolo = new Rettangolo();
+
+            cerchio.Raggio = 3;
+            rettangolo.Lato1 = 2;
+            rettangolo.Lato2 = 4;
+
+            Console.WriteLine(cerchio.ToString());
+            Console.WriteLine(rettangolo.ToString());
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ esercizio override/Rettangolo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace esercizio_override
+{
+    internal class Rettangolo
+    {
+        public int Lato1 { get; set; }
+        public int Lato2 { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Lato1, Lato2);
+        }
+
+
+    }
+}
